7c11c58fd9729da1b2ca486304a553d2eb9f82e6
Cristina <c.neacsu@lancaster.ac.uk>
2024-06-22 15:26:29 +0200

    pushhhhhhhhhhhhhhhhhh

--- lana-coded-fe/src/pages/HomePage.tsx
+++ lana-coded-fe/src/pages/HomePage.tsx
@@ -1,11 +1,33 @@
-import {Button, Stack} from "@mui/material";
+import {Button, Dialog, DialogContent, DialogTitle, Stack, TextField, useTheme} from "@mui/material";
+import {useState} from "react";
 
 function HomePage() {
+  const [name, setName] = useState("name");
+  const theme = useTheme()
+  const [isDialogOpen, setIsDialogOpen] = useState(false)
+
   return (
     <Stack justifyContent={"center"} alignItems={"center"} height={"720px"}>
-      <Button variant={"contained"}>
+      <Button variant={"contained"} onClick={() => setIsDialogOpen(true)}>
         apasa mi pula cu nasul rawr
       </Button>
+
+      <TextField
+        variant={"filled"}
+        label={"Name"}
+        placeholder={"Please enter name..."}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <Dialog open={isDialogOpen} onClose={() => setIsDialogOpen(false)} sx={{backgroundColor:theme.palette.background.default}}>
+        <DialogTitle>
+          Sugi pula
+        </DialogTitle>
+        <DialogContent>
+          Esti un muist si un poponar
+        </DialogContent>
+      </Dialog>
     </Stack>
   );
 }
